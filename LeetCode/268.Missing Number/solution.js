4f8a0f0cf8eef0592ae0b25387c1eceaa52fe3b7
Luke Dong <52880816+pig84720@users.noreply.github.com>
2019-10-02 16:02:41 +0800

    Update solution.js

--- LeetCode/268.Missing Number/solution.js
+++ LeetCode/268.Missing Number/solution.js
@@ -6,15 +6,9 @@ var missingNumber = function(nums) {
     var num = 0;
     var numm = 0;
     for(i = 0; i<nums.length; i++){
-    if(nums[i] != ','){
-    num = num + nums[i];
+    num = num + nums[i];    
     }
-    //nums加總
-    else if(nums[i] === ','){
-    num = num
-    }
-    //篩掉','    
-    }
+    //題目給的數字加總
     for(i = 0; i<nums.length + 1;i++){
     numm = i + numm;    
     }
