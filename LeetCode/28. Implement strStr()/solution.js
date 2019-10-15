8fd64204e82e4fe3274cd1f52704a9b748951ccf
董宇庭 <luke.dong@mail.searching-service.com>
2019-10-15 10:40:23 +0800

    add 28. Implement strStr()

---
+++ LeetCode/28. Implement strStr()/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    //indexOf()找出第一次出現的位置
+    return haystack.indexOf(needle)
+    
+};
