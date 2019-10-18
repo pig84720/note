5254ed369e7d745fbcce6117dcebd2413c7e4372
董宇庭 <luke.dong@mail.searching-service.com>
2019-10-18 12:35:07 +0800

    add 69. Sqrt(x)

---
+++ LeetCode/69. Sqrt(x)/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    //Math.sqrt() → 開根號
+    var x = Math.sqrt(x),
+    //Math.floor() → 取小於這個數的最大整數
+        x = Math.floor(x);
+    return x
+};
