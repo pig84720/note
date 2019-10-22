250b82d8753e07a3e05d00504078dfb0b0e74038
董宇庭 <luke.dong@mail.searching-service.com>
2019-10-22 12:26:17 +0800

    add 121. Best Time to Buy and Sell Stock

--- LeetCode/121. Best Time to Buy and Sell Stock/solution.js
+++ LeetCode/121. Best Time to Buy and Sell Stock/solution.js
@@ -3,32 +3,22 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var box = [],
-        TF = false;
-    //數字太大計算時間超時 各別處理
-    if(prices[0] == 10000) {
-        return 3;
-    }
-    //第i項後所有項目依序減第i項 並記錄
+    var ans = 0;
+    //第i項後所有項依序減第i項
     for(i = 0 ;i < prices.length ;i++) {
         for(j = i + 1 ;j <prices.length ;j++) {
-            box.push(prices[j] - prices[i])
+            //紀錄第一組數字
+            if(ans.length == 0) {
+                ans = prices[j] - prices[i];
+            //若prices[j] - prices[i]大於紀錄的數字 紀錄之
+            } else if(prices[j] - prices[i] > ans) {
+                ans = prices[j] - prices[i];
+            }
         }
     }
-    //由小到大排序
-    box.sort(function (a, b) {
-        return a - b
-    })
-    for(k = 0 ;k <box.length ;k++){
-        if(box[k] > 0) {
-            TF = true;
-        }
-    }
-    //若紀錄之數字皆小於等於0代表沒得賺 回傳0
-    if(TF == false) {
+    if(ans <= 0) {
         return 0;
-    //回傳最大的數字
     } else {
-        return box[box.length - 1];
+        return ans;
     }
 };
