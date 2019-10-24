8fa1f39b13e645c2ab2317e0c57cac5f149ff816
董宇庭 <luke.dong@mail.searching-service.com>
2019-10-24 10:02:10 +0800

    add /**

---
+++ LeetCode/977. Squares of a Sorted Array/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+    //各項以各項平方取代
+    for(i = 0; i < A.length; i++) {
+        A[i] = Math.pow(A[i],2);
+    }
+    //由小到大排序
+    A.sort(function(a,b) {
+        return a - b;
+    });
+    return A
+};
