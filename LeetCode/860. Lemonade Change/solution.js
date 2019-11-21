60a0f337e92b57a83e8cb112b45d30eb7a393888
董宇庭 <luke.dong@mail.searching-service.com>
2019-11-21 14:59:52 +0800

    add 860. Lemonade Change

--- LeetCode/860. Lemonade Change/solution.js
+++ LeetCode/860. Lemonade Change/solution.js
@@ -7,24 +7,24 @@ var lemonadeChange = function(bills) {
         how_many_10 = 0;
     for(i = 0; i < bills.length; i++) {
         if(bills[i] == 5) {
-            how_many_5 = how_many_5 + 1;
+            how_many_5++;
         } else if(bills[i] == 10) {
-            how_many_10 = how_many_10 + 1;
+            how_many_10++;
             //沒五塊找給客人時
             if(how_many_5 == 0) {
                 return false;
             } else {
-                how_many_5 = how_many_5 - 1;
+                how_many_5--;
             }
         } else if(bills[i] == 20) {
             if(how_many_10 != 0) {
                 //有十塊時先拿去找
-                how_many_10 = how_many_10 - 1;
+                how_many_10--;
                 //沒五塊找給客人時
                 if(how_many_5 == 0) {
                     return false;
                 } else {
-                    how_many_5 = how_many_5 - 1;
+                    how_many_5--;
                 }
             } else {
                 //沒十塊且不足三個五塊找給客人時
