17f4253b96ef3fa0ea4539b63c99c99e271fe833
董宇庭 <luke.dong@mail.searching-service.com>
2019-11-21 17:19:08 +0800

    add 665. Non-decreasing Array

--- LeetCode/665. Non-decreasing Array/solution.js
+++ LeetCode/665. Non-decreasing Array/solution.js
@@ -14,9 +14,9 @@ var checkPossibility = function(nums) {
                 nums[i] = nums[i + 1] - 1;
             } else if(i == nums.length - 2) {
                 nums[i + 1] = nums[i];
-            } else if(nums[i + 1] < nums[i] && nums[i + 1] <　nums[i + 2]) {
+            } else if(nums[i + 1] <　nums[i + 2]) {
                 nums[i + 1] = nums[i];
-            } else if(nums[i + 1] < nums[i] && nums[i + 2] > nums[i]) {
+            } else if(nums[i + 2] > nums[i]) {
                 nums[i + 1] = nums[i];
             }
             num = num + 1;
