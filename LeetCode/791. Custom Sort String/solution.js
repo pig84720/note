52ecc62d9249a760ad0ed1e715d71f1c11bc10a5
董宇庭 <luke.dong@mail.searching-service.com>
2019-12-18 17:46:43 +0800

    add 791. Custom Sort String

---
+++ LeetCode/791. Custom Sort String/solution.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {string}
+ */
+var customSortString = function(S, T) {
+    var count = 0,
+        ans = [],
+        box = [];
+    //在空陣列中照S陣列字母順序塞入T陣列中含有S陣列內字母重複的字母
+    for(i = 0; i < S.length; i++) {
+        for(j = 0; j < T.length; j++) {
+            if(S[i] == T[j]) {
+                ans.push(S[i]);
+            }
+        }
+    }
+    //紀錄ans內字母種類在box
+    for(i = 0; i < ans.length; i++) {
+        if(i == 0) {
+            box.push(ans[i]);
+        } else if(ans[i] != ans[i - 1]) {
+            box.push(ans[i]);
+        }
+    }
+    //T陣列中只要不是box陣列中含有的字母就記錄在後面
+    for(i = 0; i < T.length; i++) {
+        for(j = 0; j < box.length; j++) {
+            if(box[j] !== T[i]) {
+                count++;
+            }
+        }
+        if(count == box.length) {
+            ans.push(T[i]);
+        }
+        count = 0;
+    }
+    return ans.join("");
+};
