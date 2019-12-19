db9e393b31378a5aa2f3d68118d17d66f587450b
董宇庭 <luke.dong@mail.searching-service.com>
2019-12-19 10:08:36 +0800

    add 442. Find All Duplicates in an Array

--- LeetCode/442. Find All Duplicates in an Array/solution.js
+++ LeetCode/442. Find All Duplicates in an Array/solution.js
@@ -3,12 +3,17 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
+    //解題思路:
+    //將陣列nums從小到大重新排列
+    //有重複的數字留下一個 其餘砍掉
     nums = nums.sort(function(a,b) {
         return a - b;
     })
     for(i = 0; i < nums.length; i++) {
         if(i == nums.length - 1) {
             nums.splice(i,1);
+        } else if(i == nums.length - 2 && nums[i] == nums[i + 1]) {
+            continue;
         } else if(nums[i] !== nums[i + 1]) {
             nums.splice(i,1);
             i--
