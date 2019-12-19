376cdf336ccf2e2777fd5956c10cb08ed49bd753
董宇庭 <luke.dong@mail.searching-service.com>
2019-12-19 11:49:42 +0800

    add 912. Sort an Array

---
+++ LeetCode/912. Sort an Array/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    nums = nums.sort(function(a,b) {
+        return a - b;
+    })
+    return nums
+};
